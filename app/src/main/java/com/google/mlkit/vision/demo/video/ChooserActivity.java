0dc3952adbd7cee41c9a541fb7c43b298d6a4486
hicham <hichamghoutani52@gmail.com>
2023-03-28 17:36:21 +0200

    add features (stream mode & import video )

---
+++ app/src/main/java/com/google/mlkit/vision/demo/video/ChooserActivity.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.mlkit.vision.demo.video;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback;
+import androidx.core.content.ContextCompat;
+import androidx.multidex.BuildConfig;
+
+//import com.google.mlkit.vision.demo.BuildConfig;
+import com.google.mlkit.vision.demo.R;
+import com.google.mlkit.vision.demo.java.CameraXLivePreviewActivity;
+import com.google.mlkit.vision.demo.java.LivePreviewActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Demo app chooser which takes care of runtime permission requesting and allow you pick from all
+ * available testing Activities.
+ */
+public final class ChooserActivity extends AppCompatActivity {
+  private static final String TAG = "ChooserActivity";
+  private static final int PERMISSION_REQUESTS = 1;
+
+
+
+//  private static final Class<?>[] CLASSES =new Class<?>[] {
+//          VideoGLESActivity.class,
+//          VideoTextureViewActivity.class,
+//          VideoRawDecoderDataActivity.class
+//  };
+
+//  private static final int[] DESCRIPTION_IDS =new int[] {
+//          R.string.desc_video_opengles_activity,
+//          R.string.desc_video_textureview_activity,
+//          R.string.desc_video_rawdecoderdata_activity
+//  };
+
+  @SuppressLint("MissingInflatedId")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    if (BuildConfig.DEBUG) {
+      StrictMode.setThreadPolicy(
+          new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+      StrictMode.setVmPolicy(
+          new StrictMode.VmPolicy.Builder()
+              .detectLeakedSqlLiteObjects()
+              .detectLeakedClosableObjects()
+              .penaltyLog()
+              .build());
+    }
+    super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate");
+
+    setContentView(R.layout.activity_chooser);
+
+
+    if (!allPermissionsGranted()) {
+      getRuntimePermissions();
+    }
+
+  }
+
+
+  public void chooseVideo(View view){
+    startActivity(new Intent(this, VideoTextureViewActivity.class));
+  }
+  public void StreamMode1(View view){
+    startActivity(new Intent(this, LivePreviewActivity.class));
+  }
+
+  private String[] getRequiredPermissions() {
+    try {
+      PackageInfo info =
+          this.getPackageManager()
+              .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+      String[] ps = info.requestedPermissions;
+      if (ps != null && ps.length > 0) {
+        return ps;
+      } else {
+        return new String[0];
+      }
+    } catch (Exception e) {
+      return new String[0];
+    }
+  }
+
+  private boolean allPermissionsGranted() {
+    for (String permission : getRequiredPermissions()) {
+      if (!isPermissionGranted(this, permission)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void getRuntimePermissions() {
+    List<String> allNeededPermissions = new ArrayList<>();
+    for (String permission : getRequiredPermissions()) {
+      if (!isPermissionGranted(this, permission)) {
+        allNeededPermissions.add(permission);
+      }
+    }
+
+    if (!allNeededPermissions.isEmpty()) {
+      ActivityCompat.requestPermissions(
+          this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+    }
+  }
+
+  private static boolean isPermissionGranted(Context context, String permission) {
+    if (ContextCompat.checkSelfPermission(context, permission)
+        == PackageManager.PERMISSION_GRANTED) {
+      Log.i(TAG, "Permission granted: " + permission);
+      return true;
+    }
+    Log.i(TAG, "Permission NOT granted: " + permission);
+    return false;
+  }
+
+
+
+}
